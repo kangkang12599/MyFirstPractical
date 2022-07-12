5a65c3b748f1b832c25fa9b25597139a6f445718
PC-65 <kangkang12599@gmail.com>
2022-07-12 17:39:58 +0800

    Week 2

--- app/src/main/java/com/example/myfirstpractical/MainActivity.kt
+++ app/src/main/java/com/example/myfirstpractical/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myfirstpractical
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -24,5 +25,15 @@ class MainActivity : AppCompatActivity() {
         val randomNum = (1..6).random()
         val numberText: TextView = findViewById(R.id.numberTxt)
         numberText.text = randomNum.toString()
+        val diceImage : ImageView = findViewById(R.id.diceImage)
+        val imgSrc = when (randomNum){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(imgSrc)
     }
 }
