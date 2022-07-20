582977672ca41748be8c031687f606dcc67b5cf3
Chow Sing Hong <kangkang12599@gmail.com>
2022-07-20 23:22:05 +0800

    Week 3

--- app/src/main/java/com/example/myfirstpractical/MainActivity.kt
+++ app/src/main/java/com/example/myfirstpractical/MainActivity.kt
@@ -1,32 +1,46 @@
 package com.example.myfirstpractical
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
-    lateinit var diceImage: ImageView
-    lateinit var numberText: TextView
+    lateinit var diceImg: ImageView
+    lateinit var numberTxt: TextView
+    lateinit var nameTxt: EditText
+    lateinit var playerNameTxt: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        diceImage = findViewById(R.id.diceImage)
-        numberText = findViewById(R.id.numberTxt)
-        val rollButton: Button = findViewById(R.id.rollBtn)
+        diceImg = findViewById(R.id.diceImg)
+        numberTxt = findViewById(R.id.numberTxt)
+        nameTxt = findViewById(R.id.nameTxt)
+        playerNameTxt = findViewById(R.id.playerNameTxt)
+        val rollBtn: Button = findViewById(R.id.rollBtn)
+        rollBtn.setOnClickListener{ rollDice() }
+        val updateBtn: Button = findViewById(R.id.updateBtn)
+        updateBtn.setOnClickListener { updateName(it) }
+    }
 
-        rollButton.setOnClickListener{ rollDice() }
+    private fun updateName(view :View){
+        playerNameTxt.text = "Hi, I am " + nameTxt.text
 
+        nameTxt.text.clear()
+        nameTxt.clearFocus()
 
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0);
     }
 
     private fun rollDice(){
         Toast.makeText(this, "!Roll", Toast.LENGTH_LONG).show()
 
         val randomNum = (1..6).random()
-        numberText.text = randomNum.toString()
+        numberTxt.text = randomNum.toString()
         val imgSrc = when (randomNum){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -35,6 +49,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(imgSrc)
+        diceImg.setImageResource(imgSrc)
     }
 }
